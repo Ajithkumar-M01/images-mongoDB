65ba950ac193363dbf6985b83805d1d1545d0818
Ajithkumar-M01 <ajithkumaran.srk@gmail.com>
2024-11-20 23:04:14 +0530

    major1

--- imageServer.js
+++ imageServer.js
@@ -3,60 +3,65 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import multer from "multer";
-import { GridFSBucket } from "mongodb";
-import Grid from "gridfs-stream";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+// app.use(cors());
 app.use(cors({
   origin: ["http://localhost:5173", "https://justice-league-ochre.vercel.app/"],
-  methods: ["GET", "POST"],
+  methods: ["GET", "POST"]
 }));
-
+// Define __dirname for ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const PORT = process.env.PORT || 5003;
 const uri = process.env.MONGODB_URI;
 
-const conn = mongoose.createConnection(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose
+  .connect(uri, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
-let gfs;
-conn.once('open', () => {
-  gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection('uploads');
+const imageSchema = new mongoose.Schema({
+  name: String,
+  image: Buffer,
+});
+const Image = mongoose.model("Image", imageSchema);
+
+app.get("/", async (req, res) => {
+  res.send("success!!!");
 });
 
 const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const upload = multer({ storage: storage });
 
-app.post('/upload', upload.single('image'), (req, res) => {
-  const writestream = gfs.createWriteStream({
-    filename: Date.now().toString() + '-' + req.file.originalname,
-    content_type: req.file.mimetype,
-    metadata: req.file,
-  });
-
-  writestream.write(req.file.buffer);
-  writestream.end();
-
-  writestream.on('close', (file) => {
-    res.status(200).json({ message: 'Image uploaded successfully', imageUrl: `/uploads/${file.filename}` });
-  });
+app.post("/upload", upload.single("image"), async (req, res) => {
+  const img = {
+    name: req.file.originalname,
+    image: req.file.buffer
+  };
+  try {
+    await Image.create(img);
+    res.status(200).json({
+      message: "Image uploaded successfully",
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error uploading image" });
+  }
 });
 
-app.get('/get-image', (req, res) => {
-  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-    if (!file || file.length === 0) {
-      return res.status(404).json({ message: 'Image not found' });
-    }
-
-    const readstream = gfs.createReadStream(file.filename);
-    readstream.pipe(res);
-  });
+app.get("/get-image", async (req, res) => {
+  try {
+    Image.find({}).then((data) => {
+      res.send({ status: "200 OK", data: data });
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Error getting image" });
+  }
 });
 
 app.listen(PORT, () => {
